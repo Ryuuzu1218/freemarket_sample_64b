5defb865f8b7ea6dc72d1433f4c71d5a3c372cdc
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-03-21 01:20:20 +0900

    k

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server '18.177.111.73', user: 'ec2-user', roles: %w{app db web}
+server '3.130.19.72', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
