e254289a5a13edbf4439fc667b19bf8735dab985
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-03-21 19:55:55 +0900

    マイグレーションファイル確認

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+# has_many :evaluation, dependent: :destroy
+# has_many :item_images, dependent: :destroy
+# belongs_to :transaction
+# has_many :comments, dependent: :destroy
+# belongs_to :user
+# belongs_to :category
+end
