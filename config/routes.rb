538d5ece3b5ed681cf788dd0a937bf25b5cef4ae
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-03-22 21:45:47 +0900

    controller作成、haml作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :items
-resources :users do
-  collection do
-    get '_new2'
-    get '_new3'
-    end
-  end
+  root 'mypages#index'
+  resources :mypages
+# resources :items
+# resources :users do
+#   collection do
+#     get '_new2'
+#     get '_new3'
+#     end
+#   end
 end
