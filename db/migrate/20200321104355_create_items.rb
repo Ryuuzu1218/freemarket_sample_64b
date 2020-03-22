265c67287a22f60fb8f19344e10ed1c5a9d1b96c
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-03-22 18:26:58 +0900

    マイグレーションファイル修正
    ・オプションの感覚を統一しました。
    ・nameのindexの空き方を修正しました。

--- db/migrate/20200321104355_create_items.rb
+++ db/migrate/20200321104355_create_items.rb
@@ -1,17 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string      :name,null: false, index
-      t.integer     :likes,null: false
-      t.reference   :category,null: false, foreign_key: true
-      t.reference   :user,null: false, foreign_key: true
-      t.integer     :price,null: false
+      t.string      :name,            null: false, index: true
+      t.integer     :likes,           null: false
+      t.reference   :category,        null: false, foreign_key: true
+      t.reference   :user,            null: false, foreign_key: true
+      t.integer     :price,           null: false
       t.text        :explanation
       t.string      :brand
-      t.string      :condition,null: false
-      t.string      :delivery_charge,null: false
-      t.string      :shipping_origin,null: false
-      t.integer     :sending_days,null: false
+      t.string      :condition,       null: false
+      t.string      :delivery_charge, null: false
+      t.string      :shipping_origin, null: false
+      t.integer     :sending_days,    null: false
       t.timestamps
     end
   end
