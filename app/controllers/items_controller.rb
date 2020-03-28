cbd1bea3cbd1108e1d88a0ac37771227eb057493
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-03-28 16:35:39 +0900

    複数画像２
    複数投稿のためのフォーム作成を一部変更
    一旦セーブのためにコミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,29 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.item_images.new
   end
+
+  def create
+     @item= Item.new(item_params)
+     if @item.save
+      redirect_to root_path
+  else
+    render :new
+  end
+end
+
 
   def show
   end
 
   def edit
   end
+
+  private
+
+def item_params
+  params.require(:item).permit(:name, :price, :explanation,:condition,:deliverry_charge,:shipping_origin,:sending_days,item_images_attributes: [:image])
+end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
 class Item < ApplicationRecord
 # has_many :evaluation, dependent: :destroy
   has_many :item_images, dependent: :destroy
-  accepts_nested_attributes_for :item_images
+  accepts_nested_attributes_for :item_images,allow_destroy: true
 # belongs_to :transaction
 # has_many :comments, dependent: :destroy
 # belongs_to :user
 # belongs_to :category
 end
+# =exh.label :image ,class:'label' do
+#   =exh.fields_for :item_images do |i|
+#    .ex__image--upload--area
+#     =i.file_field :image, class: 'none'
+#     =icon('fa','camera')
+#     クリックして写真をアップロード
