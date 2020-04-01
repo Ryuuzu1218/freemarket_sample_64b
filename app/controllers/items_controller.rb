64337afe92b9e76df533ceb6434bca977b2e840d
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-04-01 13:14:59 +0900

    微調整
    items_controllerのインデントを修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @parents = Category.all.where(ancestry:nil).order("id ASC").limit(13)
   end
 
   def new
+    @parents = Category.all.where(ancestry:nil).order("id ASC").limit(13)
     @item = Item.new
     @item.item_images.new
   end
 
   def create
-     @item= Item.new(item_params)
-     if @item.save
-      redirect_to root_path
-  else
-    render :new
-  end
+    @item= Item.new(item_params)
+    if @item.save
+     redirect_to root_path
+    else
+     render :new
+    end
 end
 
 
@@ -32,7 +34,7 @@ end
   end
   private
 
-def item_params
-  params.require(:item).permit(:name, :price, :explanation,:condition,:deliverry_charge,:shipping_origin,:sending_days,item_images_attributes: [:image,:_destroy, :id])
-end
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation,:condition,:deliverry_charge,:shipping_origin,:sending_days,item_images_attributes: [:image,:_destroy, :id])
+  end
 end
