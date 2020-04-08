0e862e627ce372be5f97ae2ac50663bda7f06207
tomohiro0310 <momonu1990@gmail.com>
2020-04-08 22:55:04 +0900

    deviseの値が登録されるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_aciton :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :money, :image, :point])
+  end
 
   private
   def production?
