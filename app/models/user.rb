681545b6c910d60a44b7fce5cd4536bc82c8529f
tomohiro0310 <momonu1990@gmail.com>
2020-04-08 23:24:02 +0900

    userモデルのメールアドレス以外のバリデーションの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 以下バリデーション
+  # 空ではないこと
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :money, :point, presence: true
+
+  # 一意性であること
+  validates :nickname, uniqueness: true
+
+  # 一致していること, ７文字以上であること
+  validates :password, confirmation: true, length: { minimum: 7 }
+
+  # メールアドレスは@とドメインを含むこと
+
 end
