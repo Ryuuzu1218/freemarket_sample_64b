95684493ab77ff5b4953609134fa4925d0f1dc44
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-04-09 20:52:03 +0900

    users-column_create

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(version: 2020_04_08_130125) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birthday", null: false
+    t.string "password", null: false
+    t.integer "money", default: 0
+    t.string "image"
+    t.integer "point", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
