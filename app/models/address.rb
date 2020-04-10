563de2ea35c76f7434e9c92976618210eb887770
tomohiro0310 <momonu1990@gmail.com>
2020-04-10 21:44:54 +0900

    add validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user  
+
+  # バリデーション
+  validate :sending_first_name, :sending_last_name, :sending_first_name_kana, :sending_last_name_kana, :postal_code, :prefecture, :city, :town, presence: true
 end
