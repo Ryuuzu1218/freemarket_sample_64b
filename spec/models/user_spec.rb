8c28b2caa8d47f82b60b6601cef070861fdbda83
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-04-13 19:40:53 +0900

    create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "is valid with a nickname, email, password" do
+    it "is valid with a nickname, email, password, first_name, last_name" do
       user = build(:user)
       expect(user).to be_valid
     end
@@ -31,12 +31,12 @@ describe User do
     it "it invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("can't be blank")
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
     
     # パスワードが７文字以上で登録ができる
     it "is vaild with a password that has more than 7 characters" do
-      user = build(:user, password: "p1ck10t", password_confirmation: "p1ck10t")
+      user = build(:user, password: "1k2o334", password_confirmation: "1k2o334")
       user.valid?
       expect(user).to be_valid
     end
@@ -45,7 +45,7 @@ describe User do
     it "is invaild with a password that has less than 6 characters" do
       user = build(:user, password: "10ako", password_confirmation: "10ako")
       user.valid?
-      expect(user.errors[:password]).to include("")
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)", "is too short (minimum is 7 characters)")
     end
 
     # 名字が空では登録ができない
@@ -62,31 +62,35 @@ describe User do
       expect(user.errors[:last_name]).to include("can't be blank")
     end
 
-    # 
+    # 名字(カナ)が空では登録ができない
     it "is invalid without a first_name_kana" do
       user = build(:user, first_name_kana: nil)
       user.valid?
       expect(user.errors[:first_name_kana]).to include("can't be blank")
     end
 
+    # 名前(カナ)が空では登録ができない
     it "is invalid without a last_name_kana" do
       user = build(:user, last_name_kana: nil)
       user.valid?
       expect(user.errors[:last_name_kana]).to include("can't be blank")
     end
 
+    # 誕生日が空では登録ができない
     it "is invalid without a birthday" do
       user = build(:user, birthday: nil)
       user.valid?
       expect(user.errors[:birthday]).to include("can't be blank")
     end
 
+    # が空では登録ができない
     it "is invalid without a money" do
       user = build(:user, money: nil)
       user.valid?
       expect(user.errors[:money]).to include("can't be blank")
     end
 
+    # が空では登録ができない
     it "is invalid without a point" do
       user = build(:user, point: nil)
       user.valid?
@@ -95,38 +99,60 @@ describe User do
 
     # パスワードに英字、数字が含まれているか
     it "is valid with a password that contains letters and numbers" do
-      user = build(:user, password: "1t0oka")
+      user = build(:user, password: "1t0okaa", password_confirmation: "1t0okaa")
       user.valid?
       expect(user).to be_valid
     end
 
-    # パスワードが数字のみのため登録できない
-    it "is invalid with a password expect for numbers" do
-      user = build(:user, password: 1234567)
+    # 氏名は全角で入力でしているか
+    it "is valid with a first_name entered full-width" do
+      user = build(:user, first_name: "ぁ-んァ-ン一-龥")
       user.valid?
-      expect(user.errors[:password][0]).to include("")
+      expect(user).to be_valid
     end
 
-    # パスワードが英字のみのため登録ができない
-    it "is invalid with a password expect for alphabet" do
-      user = build(:user, password: kokekko)
+    # 名前は全角で入力でしているか
+    it "is valid with a last_name entered full-width" do
+      user = build(:user, last_name: "ぁ-んァ-ン一-龥")
       user.valid?
-      expect(user.errors[:password][0]).to include("")
+      expect(user).to be_valid
     end
 
-    # 名字をひらがなでは登録できない
-    it "is invalid with a first_name_kana that katakana" do
-      user = build(:user, first_name_kana: "あ")
+    # 氏名(カナ)は全角で入力でしているか
+    it "is valid with a first_name_kana entered full-width" do
+      user = build(:user, first_name_kana: "サカエ")
       user.valid?
-      expect(user.errors[first_name_kana][0]).to include("")
+      expect(user).to be_valid
     end
 
-    # 名前をひらがなでは登録できない
-    it "is invalid with a last_name_kana that katakana" do
-      user = build(:user, last_name_kana: "あ")
+    # 名前(カナ)は全角で入力でしているか
+    it "is valid with a last_name_kana entered full-width" do
+      user = build(:user, last_name_kana: "タロウ")
       user.valid?
-      expect(user.errors[last_name_kana][0]).to include("")
+      expect(user).to be_valid
     end
-  end
 
+    # ＠を含んで使用しているか
+    it "is valid with a email that includes @" do
+      user = build(:user, email: "aaa@aaa.jp")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # ドメインを含んで使用しているか
+    it "is valid with a email that includes ne.jp" do
+      user = build(:user, email: "aaa@aaa.ne.jp")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # ドメインを含んで使用しているか
+    it "is valid with a email that includes com" do
+      user = build(:user, email: "aaa@aaa.com")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+
+end
 end
