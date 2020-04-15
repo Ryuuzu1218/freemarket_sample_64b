7e7a82ea24e51522778ee9e33832de6cc9bd3f4d
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-04-15 20:48:08 +0900

    Revert "Revert "微調整""
    This reverts commit ea2626beac581888e1970b594a2d93198e83b33c.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2020_04_13_151003) do
+ActiveRecord::Schema.define(version: 2020_04_08_130125) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prefecture", null: false
