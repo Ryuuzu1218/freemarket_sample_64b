6be553da22707fe2e1e59def37f1994518506e87
tomohiro0310 <momonu1990@gmail.com>
2020-04-15 20:30:36 +0900

    addressテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_141640) do
+ActiveRecord::Schema.define(version: 2020_04_15_112738) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "prefecture", null: false
     t.string "city", null: false
     t.string "town", null: false
     t.string "building"
