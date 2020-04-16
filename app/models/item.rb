1300d89f797d7c6b224b4d555f03b77bde8e81ab
tomohiro0310 <momonu1990@gmail.com>
2020-04-16 23:52:32 +0900

    配送元のアソシエーションが抜けていたため追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :sending_days
+  belongs_to_active_hash :shipping_origin
 # has_many :evaluation, dependent: :destroy
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images,allow_destroy: true
