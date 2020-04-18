806695bdbb23519664b620292f063d0dd42b074f
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-04-18 20:25:06 +0900

    修正

--- db/migrate/20200418110930_change_column_to_items.rb
+++ db/migrate/20200418110930_change_column_to_items.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToItems < ActiveRecord::Migration[5.2]
   def change
-    change_column :items, :likes, :interger, null: true, default: 0
+    change_column :items, :likes, :interger, default: 0
   end
 end
