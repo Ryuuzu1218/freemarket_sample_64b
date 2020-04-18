858eef964eeae187bb0036e38a02257712d984ea
tomohiro0310 <momonu1990@gmail.com>
2020-04-18 21:55:48 +0900

    fix spell miss

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -6,20 +6,20 @@ $(function() {
   // 子カテゴリー用html
   function childrenBox(buildHTML){
     let childSelectHtml = '';
-    childSelectHtml = `<select class='ex__detail__category--select' id='child-form' name = 'category_id'>
-                        <option value="選択してください" data-category="選択してください"></option>
+    childSelectHtml = `<select class='ex__detail__category__box--select select' id='child-form' name = 'category_id'>
+                        <option value="選択してください" data-category="選択してください">選択してください</option>
                           ${buildHTML}
                         <select>`;
-    $('.ex__detail__category').append(childSelectHtml);
+    $('.ex__detail__category__box').append(childSelectHtml);
   }
   // 孫カテゴリー用html
   function grandchildrenBox(buildHTML){
     let grandchildSelectHtml = '';
-    grandChildSelectHtml = `<select class='ex__detail__category--select' id='grandchild-form' name = 'category_id'>
-                              <option value="選択してください" data-category="選択してください"></option>
+    grandChildSelectHtml = `<select class='ex__detail__category__box--select' id='grandchild-form' name = 'category_id'>
+                              <option value="選択してください" data-category="選択してください">"選択してください"</option>
                                 ${buildHTML}
                               <select>`;
-    $('.ex__detail__category').append(grandchildSelectHtml);
+    $('.ex__detail__category__box').append(grandchildSelectHtml);
   }
 
   //親カテゴリー選択時イベント
@@ -43,7 +43,6 @@ $(function() {
           buildHTML += options(child);
         });
         childrenBox(buildHTML);
-        console.log();
       })
       .fail(function() {
         alert('カテゴリー取得に失敗しました');
@@ -55,8 +54,8 @@ $(function() {
   });
 
   // 子カテゴリー選択時
-  $('#child-form').on('change', function(){
-    var childId = $('#child_form option:selected').data('category'); //選択された子カテゴリーのidを取得
+  $('.ex__detail__category__box').on('change', '#child_form', function(){
+    var childId = $('#child-form option:selected').data('category'); //選択された子カテゴリーのidを取得
     if (childId != "選択してください"){ //子カテゴリーが初期値でない時
       $.ajax({
         url: 'category_grandchildren',
