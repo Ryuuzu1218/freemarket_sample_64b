74e728c97b363b107f4e420c52c8c92c54caa33e
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-04-19 23:36:33 +0900

    画像アップロードエラー原因探し
    ユーザー登録、商品出品の画像がアップロードできない不具合の原因究明。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+before_action :move_to_index, except: [:index, :show]
   def index
     @parents = Category.all.where(ancestry:nil).order("id ASC").limit(13)
     @items = Item.includes(:item_images).order('created_at DESC').limit(6).where.not(transaction_status: 0).where(transaction_status: 1)
@@ -12,6 +13,7 @@ class ItemsController < ApplicationController
 
   def create
     @item= Item.new(item_params)
+    binding.pry
     if @item.save
      redirect_to root_path
     else
@@ -37,6 +39,10 @@ end
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :explanation,:condition_id,:delivery_charge_id,:shipping_origin_id,:sending_days_id,item_images_attributes: [:image,:_destroy, :id]).merge(user_id:current_user.id)
+    params.require(:item).permit(:name, :price, :explanation,:condition,:deliverry_charge,:shipping_origin,:sending_days,item_images_attributes: [:image,:_destroy, :id])
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
