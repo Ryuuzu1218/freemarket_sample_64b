c1691b88b2474a5252f777bc0ead60b2a2806c08
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-04-20 23:25:22 +0900

    unnecessary_test

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -9,13 +9,6 @@ describe Users::RegistrationsController do
     end
   end
 
-  # describe 'POST #create' do
-  #   it 'a' do
-  #     request.env["devise.mapping"] = Devise.mappings[:user]
-  #     post :create
-  #     expect(response).to render_template :new
-  #   end
-  # end
 
   describe 'POST #create' do
     before do
@@ -44,36 +37,8 @@ describe Users::RegistrationsController do
     end
   end
 
-  describe 'POST #new3' do
-    before do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      session["devise.regist_data"] == {user: @user}.clear
-      binding.pry
-    end
-      context '有効なデータの場合' do
-        subject{
-          address_params = attributes_for(:address)
-          post :new3, params: { address: address_params},
-                                  session: {
-                                    sending_first_name:             "栄",
-                                    sending_last_name:              "太郎",
-                                    sending_first_name_kana:        "サカエ",
-                                    sending_last_name_kana:         "タロウ",
-                                    postal_code:                    "1234567",
-                                    prefecture_id:                  "23",
-                                    city:                           "名古屋市中区",
-                                    town:                           "3-2",
-                                  }
-        }
-        it "" do
-          subject
-          expect(response).to render_template :new_address
-        end
-    end
-  end
-
   describe 'POST #create' do
-    it '' do
+    it 'sessionに保存されて遷移しているか' do
       request.env["devise.mapping"] = Devise.mappings[:user]
       @user = attributes_for(:user)
       session["devise.regist_data"] = {user: @user}
@@ -82,7 +47,7 @@ describe Users::RegistrationsController do
     end
   end
   describe 'GET #new3' do
-    it '' do
+    it 'sessionに保存されて遷移しているか' do
       request.env["devise.mapping"] = Devise.mappings[:user]
       @user = attributes_for(:user)
       session["devise.regist_data"] = {user: @user}
