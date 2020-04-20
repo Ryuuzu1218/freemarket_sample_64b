f8008e2c71922740ab554d4ba1cbfe000c2ed59f
tomohiro0310 <momonu1990@gmail.com>
2020-04-21 01:50:20 +0900

    リロードしないとjavascriptが動かない問題の解決、変数がうまくいってなかったため戻した

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,16 +1,12 @@
-// グローバルスコープ
-childSelcector = $('#child-form')
-grandchildSelector = $('#grandchild-form')
-
-$(function() {
+$(document).on('turbolinks:load', function() {
   function options(category){ // option生成
-    let html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    let html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
     return html;
   }
   // 子カテゴリー用html
   function childrenBox(buildHTML){
     let childSelectHtml = '';
-    childSelectHtml = `<select class='ex__detail__category__box--select select' id='child-form' name = 'category_id'>
+    childSelectHtml = `<select class='ex__detail__category__box--select select' id='child-form' name = 'item[category_id]'>
                         <option value="選択してください" data-category="選択してください">選択してください</option>
                           ${buildHTML}
                         <select>`;
@@ -19,30 +15,30 @@ $(function() {
   // 孫カテゴリー用html
   function grandchildrenBox(buildHTML){
     let grandchildSelectHtml = '';
-    grandchildSelectHtml = `<select class='ex__detail__category__box--select select' id='grandchild-form' name = 'category_id'>
+    grandchildSelectHtml = `<select class='ex__detail__category__box--select select' id='grandchild-form' name = 'item[category_id]'>
                               <option value="選択してください" data-category="選択してください">選択してください</option>
                                 ${buildHTML}
                               <select>`;
     $('.ex__detail__category__box').append(grandchildSelectHtml);
-    console.log(grandchildSelectHtml)
+    console.log(grandchildSelectHtml);
   }
 
   //親カテゴリー選択時イベント
   $('#parent-form').on("change", function() {
-    let parentValue = document.getElementById('parent-form').value;
-    if (parentValue != '選択してください') { //親の値が初期値でない時
+    let parentId = document.getElementById('parent-form').value;
+    if (parentId != '選択してください') { //親の値が初期値でない時
       $.ajax({ //ajax通信
         url: 'category_children',
         type: 'GET',
         data: {
-          parent_name: parentValue
+          parent_id: parentId
         },
         dataType: 'json'
       })
       //成功時
       .done(function(children){
-        childSelcector.remove(); // 子を削除
-        grandchildSelector.remove(); // 孫を削除
+        $('#child-form').remove(); // 子を削除
+        $('#grandchild-form').remove(); // 孫を削除
         let buildHTML = '';
         children.forEach(function(child){
           buildHTML += options(child);
@@ -53,15 +49,15 @@ $(function() {
         alert('カテゴリー取得に失敗しました');
       })
     }else{ // 親が初期値の時
-      childSelcector.remove(); 
-      grandchildSelector.remove();
+      $('#child-form').remove(); 
+      $('#grandchild-form').remove();
     }
   });
 
   // 子カテゴリー選択時
   $(document).on('change', '#child-form', function(){
-    let childId = $('#child-form option:selected').data('category');
-    if (childId != ''){ //子カテゴリーが初期値でない時
+    let childId = document.getElementById('child-form').value;
+    if (childId != '選択してください'){ //子カテゴリーが初期値でない時
       $.ajax({
         url: 'category_grandchildren',
         type: 'GET',
@@ -69,20 +65,18 @@ $(function() {
         dataType: 'json'
       })
       .done(function(grandchildren){
-        if (grandchildren.length != 0) {
-          grandchildSelector.remove();
-          let buildHTML = '';
-          grandchildren.forEach(function(grandchild){
-            buildHTML += options(grandchild);
-          });
-          grandchildrenBox(buildHTML);
-        }
+        $('#grandchild-form').remove();
+        let buildHTML = '';
+        grandchildren.forEach(function(grandchild){
+          buildHTML += options(grandchild);
+        });
+        grandchildrenBox(buildHTML);
       })
       .fail(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      grandchildSelector.remove();
+      $('#grandchild-form').remove();
     }
   });
 });
