14f7ea6b9f934a0cc2c4e52e250b091b03b0dfef
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-04-23 21:43:12 +0900

    バリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images,allow_destroy: true
   belongs_to :user
   belongs_to :category
+
+  validates :name, :user_id, :item_images, :category_id, :price, :transaction_status, :sending_days_id, :shipping_origin_id, :delivery_charge_id, :condition_id, presence: true
+  validates :explanation, presence: true, length: { maximum: 1000 }
+
 end
