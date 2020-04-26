5e6d55cdc0d6af88f4d92ad8be92dc9e47c9ed0b
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-04-26 18:45:51 +0900

    出品の単体テスト　モデル

---
+++ spec/factories/item.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :item do
+    
+    user_id                  {"1"}
+    name                     {"柑橘"}
+    explanation              {"酸っぱいのを好むか甘いのを好むかで評価が分かれる"}
+    category_id              {"365"}
+    brand                    {"あきひめ"}
+    condition_id             {"1"}
+    delivery_charge_id       {"1"}
+    shipping_origin_id       {"24"}
+    sending_days_id          {"2"}
+    price                    {"3000"}
+  end
+end 
