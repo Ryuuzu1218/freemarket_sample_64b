1be26daacdea943b987936ed83b1d028d8953d3c
tomohiro0310 <momonu1990@gmail.com>
2020-04-29 20:41:40 +0900

    commit1

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,9 @@
 class CardController < ApplicationController
   def new
+    # card登録
   end
 
   def show
+    # 登録したクレジットカードの表示
   end
 end
---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,7 @@
+class TransactionsController < ApplicationController
+  def buy
+  end
+
+  def transaciton_end
+  end
+end
