5100e11f43902438f34cfd5d56c418f57a19aaa1
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-04-29 14:33:03 +0900

    4/29 14:30
    コントローラテストでfactoryの’データが入ることを確認

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe ItemsController do
+describe ItemsController, type: :controller  do
   describe 'GET #new' do
     it "new.html.hamlに遷移する" do
       get :new
@@ -8,27 +8,15 @@ describe ItemsController do
     end
 
 describe 'POST #create' do
-
     it "データが入力されているか？" do
-    post :create, params: { 
-      item_images: [build(:item_image)],
-      user_id: '1',
-      name: '柑橘',
-      explanation: '酸っぱいのを好むか甘いのを好むかで評価が分かれる',
-      category_id: '365',
-      brand: 'あきひめ',
-      condition_id: '1',
-      delivery_charge_id: '1',
-      shipping_origin_id: '24',
-      sending_days_id: '2',
-      price: '3000'
-    }
-    it "" do
-      subject
-      expect(response).to render_template :index
+    item=build(:item)
     end
 
-  end
-  end
+    it "" do
+      subject
+      expect(response).to redirect_to :index
+    end
+
+end
 end
 end
