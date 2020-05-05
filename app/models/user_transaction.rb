d6bb40433c3c2c1fd967d7e30dd0c8b527081756
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-05-05 21:31:01 +0900

    Update user_transaction.rb

--- app/models/user_transaction.rb
+++ app/models/user_transaction.rb
@@ -1,4 +1,4 @@
-class UserTransactiom < ApplicationRecord
+class UserTransaction < ApplicationRecord
   belongs_to :buyer, class_name: 'User'
   belongs_to :saler, class_name: 'User'
   belongs_to :transaction
