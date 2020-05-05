bc5090e6c413748f343f0c9678fa92e72235b355
tomohiro0310 <momonu1990@gmail.com>
2020-05-05 14:21:43 +0900

    payjp.jsを修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,28 +1,29 @@
-$(document).on('turbolinks:load', function() {
-  Payjp.setPublicKey('');
-  %("#charge-form").on('click', function(e){
+window.addEventListener('DOMContentLoaded', function(){
+  let submit = document.getElementById("payment_card_submit-button");
+  Payjp.setPublicKey('pk_test_11a1cbafd4b6f03d6a6e018b');
+    submit.addEventListener('click', function(e){
     e.preventDefault();
-    let card = {
-      number: $('#creditcard_card_number').val(),
-      cvc:    $('#creditcard_card_pass').val(),
-      exp_month: $('#creditcard_card_month').val(),
-      exp_year: $('#creditcard_card_year').val(),
+
+    let card = { //カード入力フォームから情報を取得
+      number: document.getElementById("").Value,
+      cvc: document.getElementById("").vlalue,
+      exp_month: document.getElementById("").vlalue,
+      exp_year: document.getElementById("").vlalue
     };
 
     Payjp.createToken(card, function(status, response){
-      if (response.error) {
-        $("#charge-form").prop('disabled', false);
-        alert("カード情報が正しくありません。");
-      }
-      else {
+      if (status === 200) {
         $(".number").removeAttr("name");
         $(".cvc").removeAttr("name");
         $(".exp_month").removeAttr("name");
         $(".exp_year").removeAttr("name");
-        let token = response.id;
-        $("#card_token").appned(`<input type= "hidden" name= "payjpToken" value= ${token}>`);
-        $("#form").get(0).submit();
+        $("#charge-form").append(
+          $('<input type="hidden" name="payjp_token>').val(response.id)
+        );
+        document.inputForm.submit();
         alert("登録が完了しました");
+      } else {
+        alert("カード情報が正しくありません。");
       }
     });
   });
