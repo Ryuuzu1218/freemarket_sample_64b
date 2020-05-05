a0d936b81f61434c62d2369906a5da0a067b2951
tomohiro0310 <momonu1990@gmail.com>
2020-05-05 20:50:29 +0900

    メソッド名でtransactionが使われているため、修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images,allow_destroy: true
   belongs_to :user
   belongs_to :category
-  belongs_to :transaction
+  has_one :tx, class_name: 'Transaction'
 
   # scope
   # カテゴリごとにアイテムを６つまで取得する
