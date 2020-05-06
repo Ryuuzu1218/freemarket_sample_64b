13496a573c83a76c71113cba8298d35880e46059
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-05-06 16:13:58 +0900

    不要なマイグレーションファイル削除

--- db/migrate/20200505125544_user_transactions.rb
+++
@@ -1,5 +0,0 @@
-class UserTransactions < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :user_transactions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_05_06_024145) do
 
-  create_table "Addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "city", null: false
     t.string "town", null: false
     t.string "building"
@@ -54,16 +54,16 @@ ActiveRecord::Schema.define(version: 2020_05_06_024145) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", default: ""
     t.integer "likes", default: 0, null: false
-    t.bigint "category_id", null: false
-    t.bigint "user_id", null: false
-    t.integer "price", null: false
-    t.text "explanation", null: false
-    t.string "brand", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "transaction_status", default: 1, null: false
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.integer "price"
+    t.text "explanation"
+    t.string "brand", default: ""
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "transaction_status", default: 1
     t.integer "sending_days_id", null: false
     t.integer "shipping_origin_id", null: false
     t.integer "delivery_charge_id", null: false
@@ -99,13 +99,13 @@ ActiveRecord::Schema.define(version: 2020_05_06_024145) do
     t.string "last_name_kana", null: false
     t.date "birthday", null: false
     t.integer "money", default: 0
-    t.string "image"
+    t.text "image"
     t.integer "point", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "Addresses", "users"
+  add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
   add_foreign_key "item_images", "items"
   add_foreign_key "items", "categories"
