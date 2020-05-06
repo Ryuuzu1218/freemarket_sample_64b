3b5d90eccae83b2a591a2f331b8acb301dfd1049
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-05-06 16:14:54 +0900

    コントローラ追記

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,12 +2,12 @@ class CardController < ApplicationController
   require "payjp"
 
   def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists? 
+    @card = Card.where(user_id: current_user.id)
+    redirect_to card_path(curresnt_user.id) if @card.exists? 
   end
 
   def create
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     if params["payjp_token"].blank?
       redirect_to action: "new"
     else
@@ -25,7 +25,31 @@ class CardController < ApplicationController
   end
 
   def show
-    # 登録したクレジットカードの表示
+    @card = Card.find_by(user_id: current_user.id)
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customoer.retrieve(@card.customer_id)
+      @customer_card = custmoer.cards.retrieve(@card.card.id)
+      @card_brand = @customer_card.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "cards/visa.svg"
+      when "MasterCard"
+        @card_src = "cards/master-card.svg"
+      when "Saison"
+        @card_src = "cards/saison-card.svg"
+      when "JCB"
+        @card_src = "cards/jcb.svg"
+      when "American Express"
+        @card_src = "cards/american_express.svg"
+      when "Diners Club"
+        @card_src = "cards/dinersclub.svg"
+      when "Discover"
+        @card_src = "cards/discover.svg"
+      end
+    end
   end
 
   def destory
