7bc35547f15c3faef6e629f35b0ee85771a99556
tsuruhaya <a.hayakawa.0122@gmail.com>
2020-05-06 16:14:20 +0900

    payjp修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -5,10 +5,10 @@ window.addEventListener('DOMContentLoaded', function(){
     e.preventDefault();
 
     let card = { //カード入力フォームから情報を取得, フォームのidを記載する
-      number: document.getElementById("card_number").Value,
-      cvc: document.getElementById("card_cvc").vlalue,
-      exp_month: document.getElementById("card_exp_month").vlalue,
-      exp_year: document.getElementById("card_exp_year").vlalue
+      number: document.getElementById("number").value,
+      cvc: document.getElementById("cvc").value,
+      exp_month: document.getElementById("card_month").value,
+      exp_year: document.getElementById("card_year").value
     };
 
     Payjp.createToken(card, function(status, response){
@@ -28,3 +28,33 @@ window.addEventListener('DOMContentLoaded', function(){
     });
   });
 });
+
+// $(function() {
+//   Payjp.setPublicKey('pk_test_11a1cbafd4b6f03d6a6e018b');
+//   $("#charge-form").on('click', function(e){
+//     e.preventDefault();
+//     let card = {
+//         number: $('#creditcard_card_number').val(),
+//         cvc:$('#creditcard_card_pass').val(),
+//         exp_month: $('#creditcard_card_month').val(),
+//         exp_year: $('#creditcard_card_year').val()
+//     };
+
+//     Payjp.createToken(card, function(status, response) {
+//       if (response.error) {
+//         $("#charge-form").prop('disabled', false);
+//         alert("カード情報が正しくありません。");
+//       }
+//       else {
+//         $(".number").removeAttr("name");
+//         $(".cvc").removeAttr("name");
+//         $(".exp_month").removeAttr("name");
+//         $(".exp_year").removeAttr("name");
+//         let token = response.id;
+//         $("#card_token").append(`<input type="hidden" name="payjpToken" value=${token}>`);
+//         $("#form").get(0).submit();
+//         alert("登録が完了しました");
+//       }
+//     });
+//   });
+// });
