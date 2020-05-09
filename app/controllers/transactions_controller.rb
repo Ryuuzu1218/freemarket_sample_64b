f9ec13bb81a58c2883d4a9bcc9e4241ec29d25e8
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-05-09 21:36:17 +0900

    商品購入確認ページビュー実装
    商品購入画面を作成した。

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,29 @@
 class TransactionsController < ApplicationController
+  require "payjp"
+
   def confirm
-    # 購入確認
+    @card = Card.find_by(user_id: current_user.id)
+    Payjp.api_key = Rails.application.credentials.PAYJP_SECRET_KEY
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @customer_card = customer.cards.retrieve(@card.card_id)
+    @card_brand = @customer_card.brand
+    case @card_brand
+    when "Visa"
+      @card_src = "cards/visa.svg"
+    when "MasterCard"
+      @card_src = "cards/master-card.svg"
+    when "Saison"
+      @card_src = "cards/saison-card.svg"
+    when "JCB"
+      @card_src = "cards/jcb.svg"
+    when "American Express"
+      @card_src = "cards/american_express.svg"
+    when "Diners Club"
+      @card_src = "cards/dinersclub.svg"
+    when "Discover"
+      @card_src = "cards/discover.svg"
+    end
+
     @item=Item.find(params[:id])
   end
 
